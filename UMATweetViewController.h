5079acea7913721ef462e2c9fc04c486589378ad
uncmobileapps <cdav@email.unc.edu>
2012-11-28 16:10:02 -0500

    Attempting sync again...

--- UMATweetViewController.h
+++ UMATweetViewController.h
@@ -1,13 +1,13 @@
 //
-//  UMAViewController.h
+//  UMATweetViewController.h
 //  What's Up
 //
-//  Created by Chris Davis on 11/8/12.
+//  Created by Lecture on 11/28/12.
 //  Copyright (c) 2012 University of North Carolina - Chapel Hill. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface UMAViewController : UIViewController
+@interface UMATweetViewController : UIViewController
 
 @end
